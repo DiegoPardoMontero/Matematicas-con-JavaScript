2daaefa2a928ec32dfa97003a407dd7b54febe8d
Diego Pardo <diegopardomontero@gmail.com>
2021-12-18 13:28:55 -0500

    Completada 2da fase

---
+++ segundaFase.js
@@ -0,0 +1,8 @@
+let nombre = "Diego"; // Tipo de dato: String;
+let apellido = "Pardo"; //Tipo de dato: String;
+let nombrePlatzi = "DPardo"; //Tipo de dato: String;
+let edad = 17; //Tipo de dato: Number;
+let correoElectronico = "dpardo@platzi.com" //Tipo de dato: String;
+let mayorDeEdad = false; //Tipo de dato: Boolean;
+let dineroAhorrado = 3000; //Tipo de dato: Number;
+let deudas = [300, 500, 270]; //Tipo de dato: Array;
