579fdc38eead61cd219cdc7a3e93779500347d62
Diego Pardo <diegopardomontero@gmail.com>
2021-12-18 14:32:11 -0500

    Solucionada la cuarta fase de la prueba

---
+++ cuartaFase.js
@@ -0,0 +1,12 @@
+let nombre = "Diego"; // Tipo de dato: String;
+let apellido = "Pardo"; //Tipo de dato: String;
+let dineroAhorrado = 3000; //Tipo de dato: Number;
+let deudas = 1000; //Tipo de dato: Number;
+
+//Solución tercer ejercicio:
+
+let nombreCompleto = nombre + " " + apellido;
+let dineroReal = dineroAhorrado - deudas;
+
+console.log('Nombre completo: ' + nombreCompleto);
+console.log('Dinero real: ' + dineroReal);
