7143c141be2f7c8b780e0a04ae3fc8d6cd4a8765
Diego Pardo <diegopardomontero@gmail.com>
2021-12-18 14:58:42 -0500

    Agregada solucion a condicionales sin else o else if

--- condicionales.js
+++ condicionales.js
@@ -12,3 +12,20 @@ else if (tipoDeSuscripcion == 'Expert'){
 else if (tipoDeSuscripcion == 'ExpertPlus'){
     console.log('Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año');
 }
+
+// Condicionales sin usar else o else if
+
+const tipoDeSuscripcion2 = 'ExpertPlus';
+
+if (tipoDeSuscripcion2 == 'Free'){
+    console.log('Solo puedes tomar los cursos gratis');
+}
+if (tipoDeSuscripcion2 == 'Basic'){
+    console.log('Puedes tomar casi todos los cursos de Platzi durante un mes');
+}
+if (tipoDeSuscripcion2 == 'Expert'){
+    console.log('Puedes tomar casi todos los cursos de Platzi durante un año');
+}
+if (tipoDeSuscripcion2 == 'ExpertPlus'){
+    console.log('Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año');
+}
