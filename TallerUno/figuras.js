eebb427bce9d41d350a998e8866de7d66a28922a
Diego <diegopardomontero@gmail.com>
2021-12-18 20:00:14 -0500

    Agregada la logica de areas y triangulos

--- TallerUno/figuras.js
+++ TallerUno/figuras.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+//Código del cuadrado
+
+const ladoCuadrado = 5;
+console.log('Los lados del cuadrado miden: ' + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log('El perímetro del cuadrado mide: ' + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log('El área del cuadrado es: ' + areaCuadrado + "cm2");
+
+//Código de triángulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 4;
+const ladoTriangulo3 = 5;
+
+console.log(
+    'Los lados del triángulo miden: ' 
+    + ladoTriangulo1 
+    + 'cm, '    
+    + ladoTriangulo2 
+    + 'cm, ' 
+    + ladoTriangulo3 
+    + 'cm'
+);
