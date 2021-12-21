e713528460f982b7b1429631fbed497c5696f131
Diego <diegopardomontero@gmail.com>
2021-12-20 22:24:55 -0500

    Agregadas funciones para el HTML en JS

--- TallerUno/figuras.js
+++ TallerUno/figuras.js
@@ -55,3 +55,19 @@ function areaCirculo(radio){
 }
 
 console.groupEnd;
+
+// Aquí interactuamos con el HTML:
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
